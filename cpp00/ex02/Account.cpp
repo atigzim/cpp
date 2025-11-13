4d4082fdad03b1ff7d9e08026593d371464f1073
Abdelali Tigzim <atigzim@e2r13p8.1337.ma>
2025-11-13 19:52:14 +0100

    fix accont

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,4 +1,10 @@
 #include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 int Account::getNbAccounts()
 {
@@ -15,4 +21,39 @@ int Account::getTotalAmount()
 int Account::getNbWithdrawals()
 {
     return (_totalNbWithdrawals);
+}
+void Account::displayAccountsInfos()
+{
+    std::cout << _nbAccounts << std::endl;
+    std::cout << _totalAmount<< std::endl;
+    std::cout << _totalNbDeposits << std::endl;
+    std::cout << _totalNbWithdrawals << std::endl;
+}
+void	Account::makeDeposit( int deposit)
+{
+    _totalNbDeposits++;
+    _totalAmount += deposit;
+}
+int Account:: checkAmount()const
+{
+    return(getTotalAmount());
+}
+bool Account::makeWithdrawal(int withdrawal)
+{
+    if (withdrawal > getTotalAmount())
+    {
+        _totalAmount -= withdrawal;
+        _totalNbWithdrawals++;
+        return (true);
+    }
+    return(false);
+}
+void	Account::displayStatus( void ) const
+{
+        // _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";deposits:" << _nbDeposits
+              << ";withdrawals:" << _nbWithdrawals
+              << std::endl;
 }
